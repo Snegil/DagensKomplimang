6f0990c4acf1bf98b95ad5f5f48eadc69598089b
Alfons Wallin <alfons.wallin@edu.gnosjo.se>
2020-01-31 15:14:44 +0100

    Grunden tillagd till dagenskomplimang sidan, layouten kan och kommer ändras i framtiden.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="author" content="Snegil">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8" lang="sv">
+		<title>
+			Dagens Komplimang
+		</title>
+		<link rel='icon' href='favicon.ico' type='image/x-icon'/>
+		<link rel="stylesheet" type="text/css" href="normalize.css"/>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+	</head>
+	<body>
+        <div id="Wrapper">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+        </div>
+	</body>
+</html>

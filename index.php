c5258e46d19b8d595b608d0cfe55f64f3255e3a5
Snegil <144wallfin@gmail.com>
2020-02-04 18:43:41 +0100

    Designen är mer refinerad, mindre rörig, typsnittet är troligen fast och kommer ej bytas, Bakgrunds färgen är ändrad till grön för att jag gillar grön mer.  Mobil anpassning påväg.

--- index.php
+++ index.php
@@ -10,10 +10,16 @@
 		<link rel='icon' href='favicon.ico' type='image/x-icon'/>
 		<link rel="stylesheet" type="text/css" href="normalize.css"/>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700&display=swap" rel="stylesheet">
 	</head>
 	<body>
         <div id="wrapper">
-            <div id="komplimang">1</div>
+            <div id="komplimang">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus eleifend ullamcorper. Maecenas eget dolor urna.
+				<div id="links">
+				
+				</div>
+			</div>
         </div>
 	</body>
 </html>

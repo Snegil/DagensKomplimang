04a466b7ced0404109cb38da74358331555e2258
Alfons Wallin <alfons.wallin@edu.gnosjo.se>
2020-02-06 12:04:41 +0100

    Ändrade meta taggen author från Snegil till Alfons Wallin

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta name="author" content="Snegil">
+		<meta name="author" content="Alfons Wallin">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta charset="utf-8" lang="sv">
 		<title>
